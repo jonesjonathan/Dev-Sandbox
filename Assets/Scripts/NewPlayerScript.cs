89cbd57b56bb828ecf000a41aced355b4a5b8095
Jonathan (Alex) Jones <jonesjon@oregonstate.edu>
2018-05-15 22:31:52 -0700

    Messing around

---
+++ Assets/Scripts/NewPlayerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewPlayerScript : MonoBehaviour
+{
+    public float speed;
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forward = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        float sideways = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+
+        transform.Translate(sideways, 0, forward);
+
+        if(Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}

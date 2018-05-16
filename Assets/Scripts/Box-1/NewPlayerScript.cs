3ef6d49ac22af02f7f92108726169aa333065373
jonesjonathan <jonesjon@oregonstate.edu>
2018-05-16 14:22:08 -0700

    Tinkering.

--- Assets/Scripts/Box-1/NewPlayerScript.cs
+++ Assets/Scripts/Box-1/NewPlayerScript.cs
@@ -4,6 +4,14 @@ using System.Collections;
 public class NewPlayerScript : MonoBehaviour
 {
     public float speed;
+    public float sensitivity;
+
+    private void Rotation()
+    {
+        Vector3 rotate = new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * sensitivity;
+        transform.Rotate(rotate);
+    }
+
     // Use this for initialization
     void Start()
     {
@@ -16,6 +24,7 @@ public class NewPlayerScript : MonoBehaviour
         float forward = Input.GetAxis("Vertical") * Time.deltaTime * speed;
         float sideways = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
 
+        Rotation();
         transform.Translate(sideways, 0, forward);
 
         if(Input.GetKeyDown("escape"))
